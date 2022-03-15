85f18994bbbf7472132feb17077e2d935c7fcb9f
MagicianzRed <961242775@qq.com>
2022-03-15 11:01:59 +0800

    7.3.2 返回 *this 的成员函数
    1. 需要注意的是
        返回 *this 的函数可以利用这个 *this 直接用 . 运算符连续调用函数
        当成员函数定义的返回类型不是引用的时候，不会改变调用的类对象的值，只能改变临时的副本
    
    2.  设计良好的 C++ 代码常常包含大量的 do_display 类似的小函数

--- C++_Chapter7/Practice7/Practice_7_Main/Screen.h
+++ C++_Chapter7/Practice7/Practice_7_Main/Screen.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class Screen
 {
@@ -12,8 +13,23 @@ public:
     inline char Get(pos r, pos c) const;  // 显示内联函数
     Screen& Move(pos r, pos c);  // 能在之后被设为内联函数
     void SomeMember() const;
+    // 根据对象是否, 是 const 重载了 display 函数
+    Screen& Display(std::ostream &os)
+    {
+        do_display(os);
+        return *this;
+    }
+
+    const Screen& Display(std::ostream& os) const
+    {
+        do_display(os);
+        return *this;
+    }
 private:
     mutable size_t access_ctr;  // 即使在一个 const 对象内也能被修改
+    /*该函数负责显示 Screen 的内容*/
+    void do_display(std::ostream &os) const
+    { os << contents; }
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
