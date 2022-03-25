83e776c652396adc68194388f525937b8a04b289
MagicianzRed <961242775@qq.com>
2022-03-25 09:29:09 +0800

    新增 QT 基于信号量线程同步示例（未完成）

--- QT_std/QtBook/chapter13/samp13_1/qmythread.cpp
+++ QT_std/QtBook/chapter13/samp13_1/qmythread.cpp
@@ -2,11 +2,29 @@
 #include <QWaitCondition>
 #include <QMutex>
 #include <QTime>
+#include <QSemaphore>
+/* 基于信号量的线程同步 */
+const int bufferSize = 8;
+int buffer1[bufferSize];
+int buffer2[bufferSize];
+
+int curBuf = 1; // 当前正在写入的buffer
+int bufNo = 0;  // 采集的缓冲区序号
+quint8 counter = 0; // 数据生成器
+
+QSemaphore emptyBufs(2);    // 信号量，空的缓冲区个数，初始资源个数为2
+QSemaphore fullBuf;         // 满的缓冲区个数，初始资源为0
+/* 基于信号量的线程同步 */
+
+
+/* 基于 QWaiCondtion 的线程同步 */
 
 QMutex mutex;
 QWaitCondition newDataAvaliable;
 int seq = 0;
 int diceValue = 0;
+/* 基于 QWaiCondtion 的线程同步 */
+
 
 void QThreadProducer::run()
 {
@@ -37,7 +55,97 @@ void QThreadConsumer::run()
     {
         mutex.lock();
         newDataAvaliable.wait(&mutex);  // 只有先解锁 mutex，其他线程可以使用 mutex
-        emit NewValue(seq, diceValue);
+        emit this->NewValue(seq, diceValue);
         mutex.unlock();
     }
 }
+/* 基于 QWaiCondtion 的线程同步 */
+
+
+
+/* 基于信号量的线程同步 */
+
+/*用于生成*/
+void QThreadDAQ::run()
+{
+    mStop = false;      // 启动时令线程为 false
+    bufNo = 0;          // 缓冲区序号
+    curBuf = 1;         // 当前写入使用的缓冲区
+    counter = 0;        // 数据生成器
+
+    int n = emptyBufs.available();  // 返回当前信号量可用的资源个数
+    /*保证线程启动时 n == 2（保证资源个数是全部能用）*/
+    if (n < 2)
+    {
+        /*当发现由资源被占用时释放对应*/
+        emptyBufs.release(2 - n);
+    }
+    while (!mStop)
+    {
+        emptyBufs.acquire();    // 获取一个空的缓冲区
+        /*产生缓冲区的数据*/
+        for (int i = 0; i < bufferSize; i++)
+        {
+            if (curBuf == 1)
+            {
+                buffer1[i] = counter;           // 向缓冲区写入数据
+            }
+            else
+            {
+                buffer2[i] = counter;
+            }
+            counter++;      // 模拟数据采集产生数据
+            msleep(50);     // 每 50ms 产生一个数据
+        }
+        bufNo++;    // 缓冲区序号
+        if(curBuf == 1)
+        {
+            curBuf = 2;
+        }
+        else
+        {
+            curBuf = 1;
+        }
+        fullBuf.release();  // 有了一个满的缓冲区，available == 1
+    }
+}
+
+/*用于显示*/
+void QThreadShow::run()
+{
+    mStop = false;
+
+    int n = fullBuf.available(); // 检查当前可用资源个数
+
+    /*若至少有一个资源可用*/
+    if (n > 0)
+    {
+        fullBuf.acquire(n); // 获得可用资源
+    }
+    while (!mStop)
+    {
+        fullBuf.acquire();  // 使用默认值 1，阻塞等待至少有一个缓冲区被填满
+        int bufferData[bufferSize];
+        int seq = bufNo;
+
+        /*检查当前正在写入的缓冲区*/
+        if (curBuf == 1)
+        {
+            for (int i = 0; i < bufferSize; i++)
+            {
+                /*如果为 1 则 把2的缓冲区放入临时变量*/
+                bufferData[i] = buffer2[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < bufferSize; i++)
+            {
+                bufferData[i] = buffer1[i];
+            }
+        }
+        emptyBufs.release();    // 释放一个已经使用完毕的缓冲区
+        emit NewValue(bufferData, bufferSize, seq);
+    }
+}
+/* 基于信号量的线程同步 */
--- QT_std/QtBook/chapter13/samp13_1/qmythread.h
+++ QT_std/QtBook/chapter13/samp13_1/qmythread.h
@@ -3,6 +3,7 @@
 
 #include <QThread>
 
+/* 基于 QWaiCondtion 的线程同步 */
 class QThreadProducer : public QThread
 {
     Q_OBJECT
@@ -28,5 +29,36 @@ public:
 signals:
     void NewValue(int seq, int diceValue);
 };
+/* 基于 QWaiCondtion 的线程同步 */
 
+
+
+/* 基于信号量的线程同步 */
+class QThreadDAQ : public QThread
+{
+    Q_OBJECT
+private:
+    bool mStop = false; // 停止线程
+protected:
+    void run() Q_DECL_OVERRIDE;
+public:
+    QThreadDAQ();
+    void StopThread();
+};
+
+class QThreadShow : public QThread
+{
+
+private:
+    bool mStop = false; // 停止线程
+protected:
+    void run() Q_DECL_OVERRIDE;
+public:
+    QThreadShow();
+    void StopThread();
+signals:
+    void NewValue(int *data,int count, int seq);
+};
+
+/* 基于信号量的线程同步 */
 #endif // QMYTHREAD_H
